5631a19d98bb0e7876260ec47181d2244294a65f
MikeZowa <mikezowa11@gmail.com>
2024-05-20 10:27:47 +0200

    db password

--- src/main/java/zw/co/els/englishlearningsystem/utils/DatabaseUtil.java
+++ src/main/java/zw/co/els/englishlearningsystem/utils/DatabaseUtil.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class DatabaseUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/user_management";
     private static final String USER = "root";
-    private static final String PASSWORD = "d3vp@ss";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
